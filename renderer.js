96624804d58c8206aeb29303ba4cb675b6fa450c
Max Wattez <max.wattez@protonmail.com>
2021-09-21 10:54:37 +0200

    :bug: Remove Focus of element on Copy

--- renderer.js
+++ renderer.js
@@ -5,87 +5,87 @@
 // selectively enable features needed in the rendering
 // process.
 
-
 // IPC imports
-const {remote, ipcRenderer} = require('electron');
+const { remote, ipcRenderer } = require("electron");
 const ipc = ipcRenderer;
-const path = require('path');
-const fs = require('fs');
+const path = require("path");
+const fs = require("fs");
 
 // Global variables
 let INPUT_DIR = null;
 let OUTPUT_DIR = null;
-const BASE_IMG_PATH = './public/images/nodir.jpg';
+const BASE_IMG_PATH = "./public/images/nodir.jpg";
 let SELECTED_PICTURE_ROTATION = 0;
 let LIST_OF_PICTURES = [];
-const EXTENSIONS = ['.jpg', '.png', '.jpeg', '.JPG', '.PNG', '.JPEG'];
+const EXTENSIONS = [".jpg", ".png", ".jpeg", ".JPG", ".PNG", ".JPEG"];
 
 // Contextual Menu Event Handler -----------------------------------------------
-window.addEventListener('contextmenu', (event) => {
+window.addEventListener("contextmenu", (event) => {
   event.preventDefault();
-  ipc.send('show-context-menu');
-})
+  ipc.send("show-context-menu");
+});
 
 // Select File Dialogs and Callbacks -------------------------------------------
 
 // button variables
-const selectFrom = document.getElementById('dirFrom');
-const selectedFrom = document.getElementById('selectedDirFrom');
-const selectTo = document.getElementById('dirTo');
-const selectedTo = document.getElementById('selectedDirTo');
-const picture = document.getElementById('picture');
-const previous = document.getElementById('previous');
-const next = document.getElementById('next');
-const clickado = document.getElementById('clickado');
-const success = document.getElementById('success');
+const selectFrom = document.getElementById("dirFrom");
+const selectedFrom = document.getElementById("selectedDirFrom");
+const selectTo = document.getElementById("dirTo");
+const selectedTo = document.getElementById("selectedDirTo");
+const picture = document.getElementById("picture");
+const previous = document.getElementById("previous");
+const next = document.getElementById("next");
+const clickado = document.getElementById("clickado");
+const success = document.getElementById("success");
 
 // event listeners
-selectFrom.addEventListener('click', (event) => {
-  LIST_OF_PICTURES = []
-  ipc.send('openDir', true);
+selectFrom.addEventListener("click", (event) => {
+  LIST_OF_PICTURES = [];
+  ipc.send("openDir", true);
 });
-selectTo.addEventListener('click', (event) => {
-  ipc.send('openDir', false);
+selectTo.addEventListener("click", (event) => {
+  ipc.send("openDir", false);
 });
-previous.addEventListener('click', () => {
+previous.addEventListener("click", () => {
   nextOrPreviousPicture(false);
 });
-next.addEventListener('click', () => {
+next.addEventListener("click", () => {
   nextOrPreviousPicture(true);
 });
-clickado.addEventListener('click', () => {
+clickado.addEventListener("click", () => {
   copyPictureToOutputDir();
 });
 
 // callback
-ipc.on('openDir', (event, isInputDir) => {
+ipc.on("openDir", (event, isInputDir) => {
   console.log(isInputDir);
-  ipc.send('openDir', isInputDir);
+  ipc.send("openDir", isInputDir);
 });
-ipc.on('selectedDir', function(event, isInputDir, dirPath) {
-  const dirBasename = path.basename(dirPath) || null;
-  if (isInputDir) {
+ipc.on("selectedDir", function (event, isInputDir, dirPath) {
+  if (isInputDir && dirPath) {
+    const dirBasename = path.basename(dirPath);
     INPUT_DIR = dirPath;
     selectedFrom.innerHTML = dirBasename;
     if (dirPath) {
-      fs.readdir(dirPath, function(err, files) {
+      fs.readdir(dirPath, function (err, files) {
         if (err) {
-          console.error('FS.readdir error: ' + err);
-          return
+          console.error("FS.readdir error: " + err);
+          return;
         }
         files.map((file) => {
           if (EXTENSIONS.indexOf(path.extname(file)) >= 0) {
             LIST_OF_PICTURES.push(path.join(dirPath, file));
           }
-        })
+        });
 
         console.log(LIST_OF_PICTURES);
         displayPictureByIndex(0);
-      })
+      });
     } else {
       picture.src = BASE_IMG_PATH;
     }
-  } else {
+  } else if (dirPath) {
+    const dirBasename = path.basename(dirPath);
     OUTPUT_DIR = dirPath;
     selectedTo.innerHTML = dirBasename;
   }
@@ -93,11 +93,13 @@ ipc.on('selectedDir', function(event, isInputDir, dirPath) {
   chargeClickado();
 });
 
-ipc.on('prevPicture', () => nextOrPreviousPicture(false));
-ipc.on('nextPicture', () => nextOrPreviousPicture(true));
-ipc.on('rotate', (event, clockwise) => {rotatePicture(clockwise);});
+ipc.on("prevPicture", () => nextOrPreviousPicture(false));
+ipc.on("nextPicture", () => nextOrPreviousPicture(true));
+ipc.on("rotate", (event, clockwise) => {
+  rotatePicture(clockwise);
+});
 
-ipc.on('execute', () => copyPictureToOutputDir());
+ipc.on("execute", () => copyPictureToOutputDir());
 
 // Helper Functions ---------------------------------------------------------
 function displayPictureByIndex(index) {
@@ -108,31 +110,28 @@ function displayPictureByIndex(index) {
   }
 }
 
-
 function nextOrPreviousPicture(isNext) {
-  SELECTED_PICTURE_ROTATION = 0
-  picture.style.transform = 'none';
+  SELECTED_PICTURE_ROTATION = 0;
+  picture.style.transform = "none";
   const currentPicturePath = picture.getAttribute("src");
   const indexOfCurrentPicture = LIST_OF_PICTURES.indexOf(currentPicturePath);
   if (isNext) {
-    displayPictureByIndex(indexOfCurrentPicture + 1)
+    displayPictureByIndex(indexOfCurrentPicture + 1);
   } else {
-    displayPictureByIndex(indexOfCurrentPicture - 1)
+    displayPictureByIndex(indexOfCurrentPicture - 1);
   }
 }
-
 
 function rotatePicture(clockwise) {
   if (clockwise) {
     SELECTED_PICTURE_ROTATION += 90;
     picture.style.transform = `rotate(${SELECTED_PICTURE_ROTATION}deg)`;
-    picture.classList.toggle('rotated');
+    picture.classList.toggle("rotated");
   } else {
     SELECTED_PICTURE_ROTATION -= 90;
     picture.style.transform = `rotate(${SELECTED_PICTURE_ROTATION}deg)`;
   }
 }
-
 
 function chargeClickado() {
   if (INPUT_DIR != null && OUTPUT_DIR != null) {
@@ -145,25 +144,26 @@ function chargeClickado() {
 function getCurrentPicturePath() {
   const currentPicturePath = picture.getAttribute("src");
   const indexOfCurrentPicture = LIST_OF_PICTURES.indexOf(currentPicturePath);
-  return LIST_OF_PICTURES[indexOfCurrentPicture]
+  return LIST_OF_PICTURES[indexOfCurrentPicture];
 }
 
 function copyPictureToOutputDir() {
+  document.activeElement.blur();
   const currentPicturePath = picture.getAttribute("src");
-  console.log("Output dir: ", OUTPUT_DIR)
-  console.log("Pic: ", currentPicturePath)
+  console.log("Output dir: ", OUTPUT_DIR);
+  console.log("Pic: ", currentPicturePath);
 
   fs.copyFile(
     currentPicturePath, // source
     path.join(OUTPUT_DIR, path.basename(currentPicturePath)), // dest
-    function(err) {
+    function (err) {
       if (err) {
         console.error("ERROR: ", err);
-        return
+        return;
       }
-      success.classList.toggle('success');
-      setTimeout(function() {
-        success.classList.toggle('success');
+      success.classList.toggle("success");
+      setTimeout(function () {
+        success.classList.toggle("success");
       }, 1225);
     }
   );
